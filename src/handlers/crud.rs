47f6a499ceb17a411a21b3392de5e16e3ed41d59
mrkaye97 <mrkaye97@gmail.com>
2025-05-31 17:55:35 -0400

    refactor: crud module

--- src/handlers/crud.rs
+++ src/handlers/crud.rs
@@ -2,6 +2,9 @@ use serde::{Deserialize, Deserializer, Serialize};
 use std::collections::HashMap;
 use time::OffsetDateTime;
 
+use rust_decimal::prelude::ToPrimitive;
+use sqlx::{Postgres, QueryBuilder};
+
 fn empty_string_as_none<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
 where
     D: Deserializer<'de>,
@@ -300,4 +303,249 @@ impl std::fmt::Display for AnyValue {
             }
         }
     }
+}
+
+pub struct WriteableTrace {
+    trace_id: String,
+    start_time: OffsetDateTime,
+    end_time: OffsetDateTime,
+    duration_ns: Option<i64>,
+}
+
+#[derive(Clone, Debug)]
+pub struct WriteableSpan {
+    span_id: String,
+    trace_id: String,
+    parent_span_id: Option<String>,
+    name: String,
+    start_time: OffsetDateTime,
+    end_time: OffsetDateTime,
+    duration_ns: i64,
+    status_code: i32,
+    status_message: Option<String>,
+}
+
+#[derive(Clone, Debug)]
+pub struct WriteableSpanAttribute {
+    span_id: String,
+    key: String,
+    value: String,
+}
+
+pub async fn insert_traces(
+    traces: &Vec<WriteableTrace>,
+    tx: &mut sqlx::Transaction<'_, Postgres>,
+) -> Result<(), axum::http::StatusCode> {
+    let mut query_builder: QueryBuilder<Postgres> =
+        QueryBuilder::new("INSERT INTO traces (trace_id, start_time, end_time, duration_ns) ");
+
+    query_builder.push_values(traces, |mut b, trace| {
+        b.push_bind(trace.trace_id.clone())
+            .push_bind(trace.start_time)
+            .push_bind(trace.end_time)
+            .push_bind(trace.duration_ns);
+    });
+
+    let query = query_builder.build();
+    query
+        .execute(&mut **tx)
+        .await
+        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(())
+}
+
+pub async fn insert_spans(
+    spans: &Vec<WriteableSpan>,
+    tx: &mut sqlx::Transaction<'_, Postgres>,
+) -> Result<(), axum::http::StatusCode> {
+    let mut query_builder = QueryBuilder::new(
+        "INSERT INTO spans (
+                    span_id, trace_id, parent_span_id, operation_name,
+                    start_time, end_time, duration_ns, status_code, status_message
+                ) ",
+    );
+
+    query_builder.push_values(spans, |mut b, span| {
+        b.push_bind(span.span_id.clone())
+            .push_bind(span.trace_id.clone())
+            .push_bind(span.parent_span_id.clone())
+            .push_bind(span.name.clone())
+            .push_bind(span.start_time)
+            .push_bind(span.end_time)
+            .push_bind(span.duration_ns)
+            .push_bind(span.status_code)
+            .push_bind(span.status_message.clone());
+    });
+
+    let query = query_builder.build();
+
+    query
+        .execute(&mut **tx)
+        .await
+        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(())
+}
+
+pub async fn insert_span_attributes(
+    span_attributes: &Vec<WriteableSpanAttribute>,
+    tx: &mut sqlx::Transaction<'_, Postgres>,
+) -> Result<(), axum::http::StatusCode> {
+    let mut query_builder = QueryBuilder::new("INSERT INTO span_attributes (span_id, key, value) ");
+
+    query_builder.push_values(span_attributes, |mut b, attr| {
+        b.push_bind(attr.span_id.clone())
+            .push_bind(attr.key.clone())
+            .push_bind(attr.value.clone());
+    });
+
+    let query = query_builder.build();
+
+    query
+        .execute(&mut **tx)
+        .await
+        .map_err(|_| axum::http::StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(())
+}
+
+pub fn flatten_spans_and_attrs(
+    payload: &TracesRequest,
+) -> Result<
+    (
+        Vec<WriteableTrace>,
+        Vec<WriteableSpan>,
+        Vec<WriteableSpanAttribute>,
+    ),
+    axum::http::StatusCode,
+> {
+    let mut trace_id_to_start_and_end_times = HashMap::new();
+
+    for resource_span in payload.resource_spans.iter() {
+        for scope_span in resource_span.scope_spans.iter() {
+            for span in scope_span.spans.iter() {
+                let span_start_time = span
+                    .start_time()
+                    .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                let span_end_time = span
+                    .end_time()
+                    .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                let trace_id = span
+                    .trace_id_hex()
+                    .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+
+                use std::collections::hash_map::Entry;
+
+                match trace_id_to_start_and_end_times.entry(trace_id.clone()) {
+                    Entry::Vacant(e) => {
+                        e.insert((span_start_time, span_end_time));
+                    }
+                    Entry::Occupied(mut e) => {
+                        let (start_time, end_time) = e.get_mut();
+                        if *start_time > span_start_time {
+                            *start_time = span_start_time;
+                        }
+                        if *end_time < span_end_time {
+                            *end_time = span_end_time;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let mut traces: Vec<WriteableTrace> = Vec::new();
+
+    for (trace_id, (start_time, end_time)) in &trace_id_to_start_and_end_times {
+        let duration_ns = (*end_time - *start_time).whole_nanoseconds().to_i64();
+        let trace = WriteableTrace {
+            trace_id: trace_id.clone(),
+            start_time: *start_time,
+            end_time: *end_time,
+            duration_ns,
+        };
+
+        traces.push(trace);
+    }
+
+    let spans_and_attrs: Result<
+        Vec<(WriteableSpan, Vec<WriteableSpanAttribute>)>,
+        axum::http::StatusCode,
+    > = payload
+        .resource_spans
+        .iter()
+        .clone()
+        .flat_map(|resource_span| {
+            resource_span
+                .scope_spans
+                .iter()
+                .clone()
+                .flat_map(|scope_span| {
+                    scope_span.spans.iter().clone().map(|s| {
+                        let trace_id = s
+                            .trace_id_hex()
+                            .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                        let start_time = s
+                            .start_time()
+                            .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                        let end_time = s
+                            .end_time()
+                            .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                        let duration_ns = s
+                            .duration_ns()
+                            .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                        let span_id = s
+                            .span_id_hex()
+                            .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                        let status_code = s.status_code();
+                        let status_message = s.status_message().map(|s| s.to_string());
+
+                        let span = WriteableSpan {
+                            span_id: span_id.clone(),
+                            trace_id,
+                            parent_span_id: s.parent_span_id.clone(),
+                            name: s.name.clone(),
+                            start_time,
+                            end_time,
+                            duration_ns,
+                            status_code,
+                            status_message,
+                        };
+
+                        let attrs: Result<Vec<WriteableSpanAttribute>, axum::http::StatusCode> = s
+                            .attributes_map()
+                            .into_iter()
+                            .map(|(k, v)| {
+                                let span_id_for_attr = s
+                                    .span_id_hex()
+                                    .map_err(|_| axum::http::StatusCode::BAD_REQUEST)?;
+                                Ok(WriteableSpanAttribute {
+                                    span_id: span_id_for_attr,
+                                    key: k.clone(),
+                                    value: v.clone(),
+                                })
+                            })
+                            .collect();
+
+                        Ok((span, attrs?))
+                    })
+                })
+        })
+        .collect();
+
+    let spans_and_attrs = spans_and_attrs?;
+
+    let spans = spans_and_attrs
+        .iter()
+        .map(|(span, _)| span.clone())
+        .collect::<Vec<WriteableSpan>>();
+
+    let span_attributes = spans_and_attrs
+        .iter()
+        .clone()
+        .flat_map(|(_, attrs)| attrs.iter().map(|attr| attr.clone()))
+        .collect::<Vec<WriteableSpanAttribute>>();
+
+    Ok((traces, spans, span_attributes))
 }
