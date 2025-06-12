f03f73c7c979ba6f1b5ac539da8cbdd313df27de
mrkaye97 <mrkaye97@gmail.com>
2025-06-11 20:45:35 -0400

    feat: initial fe work

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { createFileRoute } from "@tanstack/react-router";
+import logo from "../logo.svg";
+import { Button } from "@/components/ui/button";
+
+export const Route = createFileRoute("/")({
+  component: App,
+});
+
+function App() {
+  return (
+    <div className="text-center">
+      <Button>Click me!</Button>
+    </div>
+  );
+}
